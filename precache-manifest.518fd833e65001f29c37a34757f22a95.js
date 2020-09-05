d315fa264ade5675fc4ce94c5fa354c997fbfaf8
Stephany Nusch <stephany.nusch@quintoandar.com.br>
2020-09-05 10:32:21 -0300

    Updates

---
+++ precache-manifest.518fd833e65001f29c37a34757f22a95.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc518c3db708a1e705168a0023bfd03e",
+    "url": "/autismo-tech/index.html"
+  },
+  {
+    "revision": "07c43e3c0d6d72c45957",
+    "url": "/autismo-tech/static/css/main.9ba219c2.chunk.css"
+  },
+  {
+    "revision": "ce38f5616831159709bb",
+    "url": "/autismo-tech/static/js/2.aa8a5fad.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/autismo-tech/static/js/2.aa8a5fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c43e3c0d6d72c45957",
+    "url": "/autismo-tech/static/js/main.9f073304.chunk.js"
+  },
+  {
+    "revision": "97e81b0a2dfc4950c909",
+    "url": "/autismo-tech/static/js/runtime-main.b70fe218.js"
+  },
+  {
+    "revision": "d3e401b770aa5d30ccbe9ba53f6cb769",
+    "url": "/autismo-tech/static/media/alura.d3e401b7.png"
+  },
+  {
+    "revision": "2601cd2881217a03c4f3ddedfc118fe2",
+    "url": "/autismo-tech/static/media/autismojobs-white.2601cd28.png"
+  },
+  {
+    "revision": "e8a72195ce1c9c8297aac2869d4153f9",
+    "url": "/autismo-tech/static/media/avanade.e8a72195.png"
+  },
+  {
+    "revision": "b1882ea8b356ddfd4c891d10c023aee8",
+    "url": "/autismo-tech/static/media/builders.b1882ea8.png"
+  },
+  {
+    "revision": "a3375c37c40db994dece29bc6b4de399",
+    "url": "/autismo-tech/static/media/ey-logo.a3375c37.png"
+  },
+  {
+    "revision": "96e244216a2d41c577914f1dd51c46d2",
+    "url": "/autismo-tech/static/media/magalu.96e24421.png"
+  },
+  {
+    "revision": "2a994941b3fea5d95a7284b454474a1f",
+    "url": "/autismo-tech/static/media/zup.2a994941.png"
+  }
+]);
